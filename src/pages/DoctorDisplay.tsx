fefca393f7f8d09f1bbbd6adf7a7ef3f820ff677
atendeplus-team <luispedrogaldino22@gmail.com>
2025-12-10 00:29:34 -0300

    Agora vai
    Esse diabo tava me pertubando a noite toda

--- src/pages/DoctorDisplay.tsx
+++ src/pages/DoctorDisplay.tsx
@@ -602,3 +602,16 @@ const DoctorDisplay = () => {
 };
 
 export default DoctorDisplay;
+text-xl text-slate-500 max-w-md mx-auto'>
+                  Adicione slides em Admin → Configurações
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorDisplay;
